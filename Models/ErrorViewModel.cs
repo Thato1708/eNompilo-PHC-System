34ca2d55cf951a8a9aacd3a5c68b4cb1ae3d9c6e
Mzileni, Olwethu, (Mr) (s220428336) <s220428336@mandela.ac.za>
2023-09-05 11:30:04 +0000

    Deleted Models

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace eNompilo.v3._0._1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

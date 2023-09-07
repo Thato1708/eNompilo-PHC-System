5244e2a80125d9cf2abccecf1ca5ed684767b47d
Kgopa, Mothupi, (Mr) (s220230226) <s220230226@mandela.ac.za>
2023-09-07 15:25:57 +0200

    added gbv functionality

---
+++ eNompilo.v3.0.1/Controllers/ReportGBvController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using eNompilo.v3._0._1.Areas.Identity.Data;
+using eNompilo.v3._0._1.Models;
+using eNompilo.v3._0._1.Models.Counselling;
+using eNompilo.v3._0._1.Models.SystemUsers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using eNompilo.v3._0._1.Models.GBV;
+
+namespace eNompilo.v3._0._1.Controllers
+{
+    public class ReportGBvController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportGBvController(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<ReportGBV> objList = _context.tblReportGBV;
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

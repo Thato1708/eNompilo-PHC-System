43cc2c804ef3a12bed92641ac459c997dc7e0681
Anam Philisani <s224103121@mandela.ac.za>
2023-09-23 13:52:18 +0200

    Added models and controllers for vax unique functionalities

---
+++ eNompilo.v3.0.1/Models/Vaccination/DoseTracking.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using eNompilo.v3._0._1.Constants;
+
+namespace eNompilo.v3._0._1.Models.Vaccination
+{
+    public class DoseTracking
+    {
+        [Key]
+        public int ID { get; set; }
+        public string VaccineAdministered { get; set; }
+        public DateOnly DateAdministered { get; set; }
+        public DateOnly? SecondDose { get; set; }
+        public VaccinableDiseases Vaccine { get; set; }
+        public string SiteAddress { get; set; }
+    }
+}
